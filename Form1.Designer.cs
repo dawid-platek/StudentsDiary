76b18a176daf37524706f91d91abde7b86abc3d3
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-11 12:24:18 +0200

    Week 5 lesson 6 - properties and events of controls

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
@@ -56,14 +56,17 @@
             this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnTest
             // 
-            this.button1.Location = new System.Drawing.Point(29, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnTest.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnTest.Location = new System.Drawing.Point(29, 27);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Konstruktor";
+            this.btnTest.UseVisualStyleBackColor = false;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            this.btnTest.MouseEnter += new System.EventHandler(this.EnterTest);
             // 
             // checkBox1
             // 
@@ -235,6 +238,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1113, 592);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.tabControl1);
@@ -249,8 +253,10 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnTest);
             this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Location = new System.Drawing.Point(149, 100);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
@@ -267,7 +273,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnTest;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace StudentsDiary
 {
@@ -15,11 +7,30 @@ namespace StudentsDiary
         public Form1()
         {
             InitializeComponent();
+            btnTest.Text = "Konstruktor";
+            btnTest.MouseLeave += BtnTest_MouseLeave;
+
+        }
+
+        private void BtnTest_MouseLeave(object sender, System.EventArgs e)
+        {
+            btnTest.Text = "OK";
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
+
+        private void btnTest_Click(object sender, System.EventArgs e)
+        {
+            btnTest.Text = "Przycisk";
+        }
+
+        private void EnterTest(object sender, System.EventArgs e)
+        {
+            btnTest.Text = "Enter";
+        }
+
     }
 }
