df579e1b76957c952315eab57f708a212783897f
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-13 23:32:23 +0200

    Week 5 lesson 8 - working with files

--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.IO;
+using System.Windows.Forms;
 
 namespace StudentsDiary
 {
@@ -8,6 +9,20 @@ namespace StudentsDiary
         {
             InitializeComponent();
 
+            var path = ($@"{Path.GetDirectoryName(Application.ExecutablePath)}\..\NowyPlik2.txt");
+
+            //if (!File.Exists(path))
+            //{
+            //    File.Create(path);
+            //}
+
+            // File.Delete(path); // usuwanie pliku
+            //File.WriteAllText(path, "Zostań Programistą .NET"); // dodanie tekstu z nadpisywaniem
+            File.AppendAllText(path, "\nAkademia .NET"); // dodanie tekstu bez nadpisywania
+
+            var text = File.ReadAllText(path);
+            MessageBox.Show(text); // wyświetlenie komunikatu w okienku
+            MessageBox.Show("Test", "Tytuł", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error); // wyświetlenie komunikatu z przyciskami w okienku
         }
 
         private void btnAdd_Click(object sender, System.EventArgs e)
