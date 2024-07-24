f5e26646e88b970736a3f414e0d0eeca84532a46
dawid-platek <dawidplatek.eu.pl@outlook.com>
2024-07-24 23:39:36 +0200

    Week 5 lesson 15 - abstraction

--- Person.cs
+++ Person.cs
@@ -1,15 +1,18 @@
 ﻿namespace StudentsDiary
 {
-    public class Person
+
+    public interface IPerson
+    {
+        string GetInfo();
+    }
+    public abstract class Person
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Comments { get; set; }
 
-        public virtual string GetInfo()
-        {
-            return string.Empty;
-        }
+        public abstract string GetInfo();
+
     }
 }
